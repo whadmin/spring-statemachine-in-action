9db702baeedc3ca8c102ad5d020a7579c735e015
wuhao10 <wuhao10@xiaomi.com>
2020-05-29 12:11:31 +0800

    first commit

---
+++ src/main/java/com/wuhao/statemachine/status/TestEvents.java
@@ -0,0 +1,9 @@
+package com.wuhao.statemachine.status;
+
+/**
+ * @Author: wuhao.w
+ * @Date: 2020/5/29 12:11
+ */
+public enum TestEvents {
+    E1,E2,E3,E4,E5,EF,EH
+}
---
+++ src/main/java/com/wuhao/statemachine/status/TestStates.java
@@ -0,0 +1,9 @@
+package com.wuhao.statemachine.status;
+
+/**
+ * @Author: wuhao.w
+ * @Date: 2020/5/29 12:10
+ */
+public enum TestStates {
+    SI,S1,S2,S3,S4,S5
+}
